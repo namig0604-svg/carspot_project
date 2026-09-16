d2ba184a5058a528a5a54245803c2e4ff3c28a78
Namig <namig0604@icloud.com>
2026-09-16 14:26:56 +0400

    Add users router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api import users
 
 app = FastAPI(title="CarSpot API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users.router, prefix="/api/users", tags=["Users"])
 
 @app.get("/")
 def root():
